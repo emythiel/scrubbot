432b1af227596fd6b6c8d510cb0f07fe4bf54c4f
emythiel <18901520+emythiel@users.noreply.github.com>
2026-02-03 14:54:13 +0100

    minor adjustments

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import { Client, Events, GatewayIntentBits } from 'discord.js'
+import { Client, Events, GatewayIntentBits } from 'discord.js';
 const token = process.env.DISCORD_TOKEN
 
 if (!token) {
-    throw new Error("Token not found")
+    throw new Error("Token not found");
 }
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once(Events.ClientReady, (readyClient) => {
+client.once(Events.ClientReady, (readyClient: Client<true>) => {
     console.log(`Ready! Logged in as ${readyClient.user.tag}`);
 })
 
