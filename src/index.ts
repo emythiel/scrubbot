efef6231f4634e8a909f9965f1a8dae13808b5f4
emythiel <18901520+emythiel@users.noreply.github.com>
2026-02-03 14:02:36 +0100

    initialize npm/typescript project structure

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { Client, Events, GatewayIntentBits } from 'discord.js'
+const token = process.env.DISCORD_TOKEN
+
+if (!token) {
+    throw new Error("Token not found")
+}
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (readyClient) => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+})
+
+client.login(token);
